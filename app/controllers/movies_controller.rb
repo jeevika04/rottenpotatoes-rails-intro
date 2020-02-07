53ddf68e2e59934ab83cbe207f114abfd4581fe5
Jeevika Jarmarwala <jeevika04@gmail.com>
2020-02-07 21:22:19 +0000

    part 2 try

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    @all_ratings = ['G', 'PG', 'PG-13', 'R'] #creates list with all the ratings to pick from
     @sort = params[:sort] #gets the sort parameter title/release_date from url
     @selected_ratings = params[:ratings] || Hash[@all_ratings.map {|x| [x, true]}]
     @all_ratings = Hash[@all_ratings.map {|x| [x, @selected_ratings.key?(x)]}]
