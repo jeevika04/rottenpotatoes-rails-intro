15b48038df4227bfad3a88e25716aa6f23621103
Jeevika Jarmarwala <jeevika04@gmail.com>
2020-02-07 20:28:40 +0000

    part 2 try

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @sort = params[:sort] || session[:sort] #gets the sort parameter title/release_date from url
-    @selected_ratings = params[:ratings] || session[:ratings] || Hash[MoviesHelper.all_ratings.map {|x| [x, true]}]
+    @sort = params[:sort] #gets the sort parameter title/release_date from url
+    @selected_ratings = params[:ratings] || Hash[MoviesHelper.all_ratings.map {|x| [x, true]}]
     @all_ratings = Hash[MoviesHelper.all_ratings.map {|x| [x, @selected_ratings.key?(x)]}]
     session[:sort] = @sort
     session[:ratings] = @selected_ratings
